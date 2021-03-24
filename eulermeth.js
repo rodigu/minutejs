40af9f8a22657969123a364132b35fe8ed64caf8
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2021-03-24 14:04:09 -0300

    Update eulermeth.js

--- eulermeth.js
+++ eulermeth.js
@@ -1,13 +1,13 @@
-function euler (x, y, dx, f, steps) {
+function euler (x, y, dx, f, steps, precision = 3) {
     let yn = y
     let xn = x
 	let ans = {}
     x += dx
     for (let i = 0; i < steps; i++) {
         y = yn + f(xn, yn) * dx
-		ans[x.toFixed(3)] = {
-			y: y.toFixed(3),
-			f: f(x, y).toFixed(3)
+		ans[x.toFixed(precision)] = {
+			y: y.toFixed(precision),
+			f: f(x, y).toFixed(precision)
 		}
         xn = x
         x += dx
